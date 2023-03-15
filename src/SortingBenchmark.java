645574c67511cf1f5189e77f197bf47ec3734c18
Manuel Keilman <se22m053@technikum-wien.at>
2023-03-15 20:51:01 +0100

    Changed Benchmark Logs

--- src/SortingBenchmark.java
+++ src/SortingBenchmark.java
@@ -1,34 +1,37 @@
 public class SortingBenchmark {
     public static void main(String[] args) {
-        int[] sizes = {10000, 50000, 100000, 500000, 1_000_000, 10_000_000};
+        int[] sizes = {10000, 50000, 100000, 500000, 1_000_000, 2_000_000};
         for (int size : sizes) {
             System.out.println("Sorting with size: " + size);
             int[] arr = ArrayGenerator.generateRandomArray(size);
 
+            //QuickSort Benchmark
             long startTime = System.currentTimeMillis();
             QuickSortSequential.quickSort(arr.clone(), 0, arr.length - 1);
             long endTime = System.currentTimeMillis();
+            long quickSequentialTime = (endTime - startTime);
             System.out.println("QuickSortSequential: " + (endTime - startTime) + "ms");
-//            for (int arrayVal :
-//                    arr) {
-//                System.out.println(arrayVal);
-//            }
 
             startTime = System.currentTimeMillis();
             QuickSortParallel.quickSort(arr.clone());
             endTime = System.currentTimeMillis();
+            long quickParallelTime = (endTime - startTime);
             System.out.println("QuickSortParallel: " + (endTime - startTime) + "ms");
+            System.out.println("QuickSort SpeedUp of: " + ((double)quickSequentialTime / (double)quickParallelTime) + "\n");
 
-
+            //MergeSort Benchmark
             startTime = System.currentTimeMillis();
             MergeSortSequential.mergeSort(arr.clone(), 0, arr.length - 1);
             endTime = System.currentTimeMillis();
-            System.out.println("MergeSortSequential: " + (endTime - startTime) + "ms");
+            long mergeSequentialTime = (endTime - startTime);
+            System.out.println("MergeSortSequential: " + mergeSequentialTime + "ms");
 
             startTime = System.currentTimeMillis();
             MergeSortParallel.mergeSort(arr.clone(), 0, arr.length - 1);
             endTime = System.currentTimeMillis();
-            System.out.println("MergeSortParallel: " + (endTime - startTime) + "ms\n");
+            long mergeParallelTime = (endTime - startTime);
+            System.out.println("MergeSortParallel: " + (endTime - startTime) + "ms");
+            System.out.println("MergeSort SpeedUp of: " + ((double)mergeSequentialTime / (double)mergeParallelTime) + "\n");
         }
     }
 }
